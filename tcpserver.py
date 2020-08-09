b4c1ec39a9cabd729bc3a7d3250298d114ca2d67
0x1729 <69423744+0x1729@users.noreply.github.com>
2020-08-09 18:41:11 +0530

    Add files via upload

---
+++ tcpserver.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import socket
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostname
+port = 4444
+
+serversocket.bind((host, port))
+
+serversocket.listen(7)
+
+while True:
+    clientsocket, address = serversocket.accept()
+
+    print("Received connection from: %s" % str(address))
+
+    message = "Hey buddy, You've been Successfully connected to the server" + "/r/n"
+
+    clientsocket.send(message)
+    clientsocket.close()
+
